7883217fa1e801053489f51bbbbc33bf81778b51
MatheusGCruz <matheuskaze@gmail.com>
2025-02-22 07:53:07 -0300

    Adjust - Analytics Style

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -146,4 +146,32 @@ import { ref, computed, defineComponent, onMounted } from "vue";
 .gridItem p{
   color:navy;
 }
+
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px silver; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: navy; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: silver; 
+}
+
+button,
+select,
+input{
+  color:navy;
+  border-radius: 10px;
+}
 </style>
